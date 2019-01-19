dcaaa68f78127cffe068b021094c5901fa0cce66
Daniel Pérez Fernández <dpefe@hotmail.es>
2019-01-19 11:21:10 +0100

    Bug Fixed and New Plugin
    Añadido nuevo plugin para ver el progreso del año y correción de errores

--- src/settings/config.js
+++ src/settings/config.js
@@ -5,18 +5,18 @@ module.exports = {
      * @type {!string}
      * //Token BotTelegram
      */
-    token_bot: '537550296:AAEyjNS3e3kcnufDKIi-YF19cNc5D6VlY78',
+    token_bot: '',
 
     /**
      * @type {!string}
      * Token Imgur
      */
-    token_imgur: '8c3d18eaf1016b8',
+    token_imgur: '',
 
 
     /**
      * @type {!string}
      * Token Pexels
      */
-    token_pexels: '563492ad6f91700001000001d6afc204966949098bc25f08265205fd'
+    token_pexels: ''
 }
