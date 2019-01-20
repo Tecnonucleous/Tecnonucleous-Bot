544bbc389e883b5c90879f2e6ff623cdde5a1c2e
Daniel Pérez Fernández <dpefe@hotmail.es>
2019-01-20 20:51:15 +0100

    Update start command
    Añadido el soporte para obtener la información que le pasamos la url del bot . Ejemplo: ?start=texto

--- src/commands/initial_commands/start.js
+++ src/commands/initial_commands/start.js
@@ -1,13 +1,20 @@
 'use strict';
 const app = require('../../settings/app')
 
-app.bot.onText(/^\/start/, function(msg){
+app.bot.onText(/\/start (.+)|\/start/i, function(msg, match){
+
+var invite_code;
 
     const prop = {
             'chat_id': msg.chat.id,
             'user_id': msg.from.id,
             'userName': msg.from.first_name
     }
-
+if (match[1] != undefined){
+invite_code = match[1];
+app.bot.sendMessage(prop.chat_id, "Hola " + prop.userName + " con código de invitación " + invite_code)
+}
+else{
     app.bot.sendMessage(prop.chat_id, "Hola " + prop.userName)
+    }
 });
