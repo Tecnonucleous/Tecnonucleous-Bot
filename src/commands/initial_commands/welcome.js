3a2f768704c69371fb493b202869ce69f6b0cc6b
Daniel Pérez Fernández <dpefe@hotmail.es>
2019-01-28 18:56:59 +0100

    Fixed welcome.js
    Arreglado el problema de las despedidas, sobraba una "s" en la condición que controlaba las despedidas

--- src/commands/initial_commands/welcome.js
+++ src/commands/initial_commands/welcome.js
@@ -13,18 +13,18 @@ app.bot.on('message', function(msg){
     }
 
     if (msg.new_chat_members != undefined){
-        
+
         const newMember = {
             'userId': msg.new_chat_member.id,
             'username': msg.new_chat_member.first_name,
             'userAlias': msg.new_chat_member.username
         }
-        
+
         app.bot.deleteMessage(prop.chat_id, prop.messageId)
         app.bot.sendMessage(prop.chat_id, "Hola " + newMember.username + ", bienvenido al grupo " + prop.chatTitle)
     }
 
-    else if (msg.left_chat_members != undefined){
+    else if (msg.left_chat_member != undefined){
 
         const leftMember = {
             'leftUserId': msg.left_chat_member.id,
@@ -35,5 +35,5 @@ app.bot.on('message', function(msg){
         app.bot.deleteMessage(prop.chat_id, prop.messageId)
         app.bot.sendMessage(prop.chat_id, leftMember.leftUsername + " abandonó el grupo")
     }
-    
+
 });
