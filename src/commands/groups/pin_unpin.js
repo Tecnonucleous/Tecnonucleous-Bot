07a0c09ad332f25813c8596b5229eb79cf909b51
Daniel Pérez Fernández <dpefe@hotmail.es>
2019-04-14 19:39:01 +0200

    Add Suport for pin messages in Basic Groups
    The update of bot API (Bot API 4.2) allows anchoring messages in basic groups 
    
    Now you can pin messages in Basic Groups

--- src/commands/groups/pin_unpin.js
+++ src/commands/groups/pin_unpin.js
@@ -4,6 +4,7 @@ const app = require('../../settings/app')
 
 app.bot.onText(/^\!pin|^\/pin/, function(msg){
     if (msg.reply_to_message == undefined){
+		app.bot.sendMessage(msg.chat.id, app.i18n.__('To use the !pin command you need to reply to the message you want to anchor'));
         return;
     };
 
@@ -19,12 +20,10 @@ app.bot.onText(/^\!pin|^\/pin/, function(msg){
 
     app.bot.getChatMember(prop.chat_id, prop.from_id).then(function(data){
         if ((data.status == 'creator')|| (data.status == 'administrator')){
-            if (prop.chatType == 'supergroup'){
+            if ((prop.chatType == 'supergroup')||(prop.chatType == 'group')){
                 app.bot.pinChatMessage(prop.chat_id, prop.replyMsg, opts);
                 app.bot.deleteMessage(prop.chat_id, prop.messageId);
             } else if (prop.chatType == 'private'){
-                app.bot.sendMessage(prop.chat_id, app.i18n.__('Command only available for supergroups'));
-            } else if (prop.chatType == 'group'){
                 app.bot.sendMessage(prop.chat_id, app.i18n.__('Command only available for supergroups'));
             }
         } else {
@@ -38,13 +37,18 @@ app.bot.onText(/^\!unpin|^\/unpin/, function(msg){
     const prop = {
         'chat_id': msg.chat.id,
         'from_id': msg.from.id,
-        'messageId': msg.message_id
+        'messageId': msg.message_id,
+		'chatType': msg.chat.type
     }
 
     app.bot.getChatMember(prop.chat_id, prop.from_id).then(function(data){
         if ((data.status == 'creator') || (data.status == 'administator')){
+			if ((prop.chatType == 'supergroup')||(prop.chatType == 'group')){
             app.bot.deleteMessage(prop.chat_id, prop.messageId);
             app.bot.unpinChatMessage(prop.chat_id)
+			} else {
+				app.bot.sendMessage(prop.chat_id, app.i18n.__('Command only available for supergroups'));
+			}
         } else {
             bot.sendMessage(prop.chat_id, app.i18n.__('Sorry, you are not an admin'));
         }
