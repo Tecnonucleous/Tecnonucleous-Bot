9ec0d84e3d44dec118c32988501e891ad69d6187
Daniel Pérez Fernández <dpefe@hotmail.es>
2020-11-13 09:18:30 +0100

    Re-added config-sample file

---
+++ src/settings/config-sample.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    /**
+     * @type {!string}
+     * //Token BotTelegram
+     */
+    token_bot: '',
+
+    /**
+     * @type {!string}
+     * Token Imgur
+     */
+    token_imgur: ''  // Optional
+}
